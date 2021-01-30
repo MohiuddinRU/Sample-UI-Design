575666001f6a87b97e7b86772b1dc9b06a6bf779
MohiuddinRU <mohiuddin.ice.ru@gmail.com>
2021-01-30 16:59:47 +0600

    Testing With Simple changes

---
+++ app/src/main/java/blog/home/mohiuddinscodes/sampleui/StoreView.java
@@ -0,0 +1,14 @@
+package blog.home.mohiuddinscodes.sampleui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_view);
+    }
+}
